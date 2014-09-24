225c40f0588f2665deef14756ae0912f490d8a35
Alex <askdurrant@gmail.com>
2014-09-24 10:52:52 +0100

    initial commit

---
+++ change-image-by-date.js
@@ -0,0 +1,10 @@
+var d = new Date();
+var year = d.getUTCFullYear();
+var month = d.getUTCMonth();
+var day = d.getUTCDate();
+var hour = d.getUTCHours();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
